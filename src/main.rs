5a21f8e5d1ecadbe6282c2b35878cf6e8053d245
Joël Lupien (Jojolepro) <jojolepro@jojolepro.com>
2020-08-25 12:14:31 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,408 @@
+use amethyst::core::frame_limiter::FrameRateLimitStrategy;
+use amethyst::ecs::*;
+use amethyst::prelude::*;
+use amethyst::shrev::{EventChannel, ReaderId};
+use amethyst::utils::*;
+//use pathfinding::directed::astar;
+use easycurses::*;
+use easycurses::constants::acs;
+use std::collections::HashMap;
+use std::time::Duration;
+use std::cmp::{min, max};
+use lazy_static::lazy_static;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Pos(pub u32, pub u32);
+
+impl Component for Pos {
+    type Storage = VecStorage<Self>;
+}
+
+pub struct Curses(pub EasyCurses);
+
+#[derive(Clone, Default, Debug, Eq, PartialEq)]
+pub struct MapCursor(pub u32, pub u32);
+
+#[derive(Clone, Default, Debug, Eq, PartialEq)]
+pub struct LayerVisibility(pub u32);
+
+// boi
+unsafe impl Send for Curses {}
+// Garanteed by the system execution scheduler
+unsafe impl Sync for Curses {}
+
+pub const GOAL: Pos = Pos(4095, 4095);
+lazy_static! {
+    static ref COLOR_NORMAL: easycurses::ColorPair = easycurses::ColorPair::new(Color::White, Color::Black);
+    static ref COLOR_EDGE: easycurses::ColorPair = easycurses::ColorPair::new(Color::Yellow, Color::Black);
+    static ref COLOR_TITLE: easycurses::ColorPair = easycurses::ColorPair::new(Color::Red, Color::White);
+    static ref COLOR_DEBUG: easycurses::ColorPair = easycurses::ColorPair::new(Color::Blue, Color::White);
+}
+
+pub struct CursesRenderSystem;
+
+impl<'a> System<'a> for CursesRenderSystem {
+    type SystemData = (
+        ReadStorage<'a, Pos>,
+        WriteExpect<'a, Curses>,
+        Read<'a, MapCursor>,
+        Read<'a, LayerVisibility>,
+    );
+    fn run(&mut self, (_positions, mut curses, cursor, layer): Self::SystemData) {
+        let curses = &mut curses.0;
+
+        // Tile space
+        // Then screenspace
+        // Then borders
+        // Then ui
+
+        // ---- Tile Space ----
+
+
+
+        let bitshift_factor = 9 * layer.0;
+        //let tiles_per_square = 1 << 9 * layer.0;
+
+        let mask: u32 = 0xFFFFFFFFu32 << bitshift_factor;
+        let mask: u32 = mask << 9; // NECESSARY TO SPLIT
+        let x_start_tile = cursor.0 & mask;
+        let y_start_tile = cursor.1 & mask;
+        //let x_start_tile = cursor.0 - (cursor.0 % tiles_per_square); // TODO: replace by bitmasking (this is the same as bitshifting right and then left??)
+        //let y_start_tile = cursor.1 - (cursor.1 % tiles_per_square);
+
+        //let x_end_tiles = x_start_tile + tiles_per_square;
+        //let y_end_tiles = y_start_tile + tiles_per_square;
+
+
+        // How many squares do we have to render for this layer?
+        //let square_count = std::u32::MAX >> 9 * (3 - layer.0);
+        let square_count = if layer.0 == 3 { 32 } else { 512 };
+
+        let layered_x_start = x_start_tile >> bitshift_factor;
+        let layered_y_start = y_start_tile >> bitshift_factor;
+        //let layered_x_stop = layered_x_start + square_count - 1;
+        //let layered_y_stop = layered_y_start + square_count - 1;
+        let layered_cursor = ((cursor.0 >> bitshift_factor) - layered_x_start, (cursor.1 >> bitshift_factor) - layered_y_start);
+        let something = bitshift_factor;
+
+        //println!("{:?}", x_start_tile);
+
+        // ---- Screen Space ----
+
+
+        let screen_offset = (0u32, 4u32);
+        let outside_edge = (20u32, 3u32);
+
+        let (screen_height, screen_width) = curses.get_row_col_count();
+        let (screen_height, screen_width) = (screen_height as u32, screen_width as u32);
+        let (xmax, ymax) = (min(screen_width as u32 - outside_edge.0, square_count), min(screen_height as u32 - outside_edge.1, square_count));
+
+
+        let render_width = screen_width - screen_offset.0 - outside_edge.0;
+        let render_height = screen_height - screen_offset.1 - outside_edge.1;
+
+        // Try to keep the cursor centered
+        // 0 <= offset <= end - render_size
+        let map_offset = (
+            min(max(0, layered_cursor.0 as i32 - (render_width >> 1) as i32), max(0, square_count as i32 - render_width as i32)) as u32,
+            //min(max(0, layered_cursor.1 as i32 - ((layered_y_stop - layered_y_start) >> 1) as i32), square_count as i32 - render_height as i32) as u32,
+            min(max(0, layered_cursor.1 as i32 - (render_height >> 1) as i32), max(0, square_count as i32 - render_height as i32)) as u32,
+        );
+
+        // Clear the screen
+        curses.set_color_pair(*COLOR_NORMAL);
+        for y in 0..screen_height {
+            for x in 0..screen_width {
+                curses.move_rc(y as i32, x as i32);
+                curses.print_char(' ');
+            }
+        }
+
+        if screen_height < 12 || screen_width < 26 {
+            curses.move_rc(0,0);
+            curses.print("Smol Screen...");
+            return;
+        }
+
+        curses.set_color_pair(*COLOR_NORMAL);
+
+        // Render the map tiles and border
+        for y in screen_offset.1..ymax {
+            for x in screen_offset.0..xmax {
+                let x_pos = x;
+                let y_pos = y;
+                curses.move_rc(y_pos as i32, x_pos as i32);
+                // TODO: Set tile color and char
+                curses.print_char('0');
+            }
+        }
+
+        // ---- Map Borders ----
+
+        // how much you need to render > the space you have available
+        let (edge_bottom, edge_top, edge_left, edge_right) = (
+            square_count - map_offset.1 > screen_height - screen_offset.1 - outside_edge.1,
+            map_offset.1 > 0,
+            map_offset.0 > 0,
+            square_count - map_offset.0 > screen_width - screen_offset.0 - outside_edge.0,
+        );
+
+        curses.set_color_pair(*COLOR_EDGE);
+
+        // Top Border
+        if edge_top {
+            for x in screen_offset.0..xmax {
+                curses.move_rc(screen_offset.1 as i32, x as i32);
+                curses.print_char('^');
+            }
+        }
+
+        if edge_left {
+            for y in screen_offset.1..ymax {
+                curses.move_rc(y as i32, screen_offset.0 as i32);
+                curses.print_char('<');
+            }
+        }
+
+        if edge_bottom {
+            for x in screen_offset.0..xmax {
+                curses.move_rc((screen_height - outside_edge.1 - 1) as i32, x as i32);
+                curses.print_char('v');
+            }
+        }
+
+        if edge_right {
+            for y in screen_offset.1..ymax {
+                curses.move_rc(y as i32, (screen_width - outside_edge.0 - 1) as i32);
+                curses.print_char('>');
+            }
+        }
+
+        // ---- UI ----
+
+        curses.set_color_pair(*COLOR_TITLE);
+
+        // Debug Info
+        curses.move_rc(0,0);
+        curses.print("World of The Fox God V0.1A");
+
+        curses.set_color_pair(*COLOR_DEBUG);
+
+        curses.move_rc(1,0);
+        curses.print(format!("Layer 0: ({},{}), Layer 1: ({},{}), Layer 2: ({},{}), Layer 3: ({},{}), Point On Map: ({},{})", 
+                             cursor.0,
+                             cursor.1,
+                             cursor.0 >> 9,
+                             cursor.1 >> 9,
+                             cursor.0 >> 18,
+                             cursor.1 >> 18,
+                             cursor.0 >> 27,
+                             cursor.1 >> 27,
+                             layered_cursor.0,
+                             layered_cursor.1));
+        curses.move_rc(3,0);
+        curses.print(format!("Current Visible Layer: {}", layer.0));
+
+        curses.move_rc(3,50);
+        curses.print(format!("Debug Data: {:?},{:?}", something, layered_x_start));
+
+        // Sidebar Test
+        curses.set_color_pair(*COLOR_NORMAL);
+        curses.move_rc(4,(screen_width - outside_edge.0) as i32);
+        curses.print("Some Things");
+        curses.move_rc(5,(screen_width - outside_edge.0) as i32);
+        curses.print("And More");
+
+
+        // Map Cursor
+
+        curses.set_color_pair(*COLOR_NORMAL);
+        curses.move_rc((layered_cursor.1 + screen_offset.1 - map_offset.1) as i32,(layered_cursor.0 + screen_offset.0 - map_offset.0) as i32);
+        curses.print_char(acs::block());
+
+        // Render
+        curses.refresh();
+    }
+}
+
+#[derive(Default)]
+pub struct CursorMoveSystem {
+    reader: Option<ReaderId<InputEvent>>,
+}
+
+impl<'a> System<'a> for CursorMoveSystem {
+    type SystemData = (
+        Write<'a, EventChannel<InputEvent>>,
+        Read<'a, LayerVisibility>,
+        Write<'a, MapCursor>,
+    );
+    fn run(&mut self, (mut input_ev, layer, mut cursor): Self::SystemData) {
+        if self.reader.is_none() {
+            self.reader = Some(input_ev.register_reader());
+        }
+        let offset = 1 << 9 * layer.0;
+        for ev in input_ev.read(&mut self.reader.as_mut().unwrap()) {
+            let new = match ev {
+                InputEvent::MoveUp => (Some(cursor.0), cursor.1.checked_sub(offset)),
+                InputEvent::MoveDown => (Some(cursor.0), cursor.1.checked_add(offset)),
+                InputEvent::MoveRight => (cursor.0.checked_add(offset), Some(cursor.1)),
+                InputEvent::MoveLeft => (cursor.0.checked_sub(offset), Some(cursor.1)),
+                _ => continue,
+            };
+            if let (Some(new_x), Some(new_y)) = new {
+                cursor.0 = new_x;
+                cursor.1 = new_y;
+            }
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct LayerVisibilityChangeSystem {
+    reader: Option<ReaderId<InputEvent>>,
+}
+
+impl<'a> System<'a> for LayerVisibilityChangeSystem {
+    type SystemData = (
+        Write<'a, EventChannel<InputEvent>>,
+        Write<'a, LayerVisibility>,
+    );
+    fn run(&mut self, (mut input_ev, mut layer,): Self::SystemData) {
+        if self.reader.is_none() {
+            self.reader = Some(input_ev.register_reader());
+        }
+        for ev in input_ev.read(&mut self.reader.as_mut().unwrap()) {
+            match ev {
+                InputEvent::LayerUp => if layer.0 < 3 { layer.0 += 1 },
+                InputEvent::LayerDown => if layer.0 > 0 { layer.0 -= 1 },
+                _ => continue,
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum InputEvent {
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    LayerUp,
+    LayerDown,
+    Cancel,
+    Accept,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Keymap {
+    pub map: HashMap<Input, InputEvent>,
+}
+
+impl Default for Keymap {
+    fn default() -> Self {
+        Keymap {
+            map: [
+                (Input::Character('h'), InputEvent::MoveLeft),
+                (Input::Character('l'), InputEvent::MoveRight),
+                (Input::Character('j'), InputEvent::MoveDown),
+                (Input::Character('k'), InputEvent::MoveUp),
+                (Input::Character(';'), InputEvent::LayerDown),
+                (Input::Character('.'), InputEvent::LayerUp),
+                (Input::Character('\n'), InputEvent::Accept),
+                (Input::Character('\u{1b}'), InputEvent::Cancel), // Escape
+            ].iter().cloned().collect(),
+        }
+    }
+}
+
+pub struct CursesInputSystem;
+
+impl<'a> System<'a> for CursesInputSystem {
+    type SystemData = (
+        Write<'a, EventChannel<InputEvent>>,
+        WriteExpect<'a, Curses>,
+        Read<'a, Keymap>,
+    );
+    fn run(&mut self, (mut input_ev, mut curses, keymap): Self::SystemData) {
+        let curses = &mut curses.0;
+        while let Some(input) = curses.get_input() {
+            if let Some(ev) = keymap.map.get(&input) {
+                input_ev.single_write(*ev);
+            }
+        }
+    }
+}
+
+impl Pos {
+    /*fn distance_bad(&self, other: &Pos) -> u32 {
+        let xdiff = if self.0 <= other.0 {
+            other.0 - self.0
+        } else {
+            self.0 - other.0
+        };
+        let ydiff = if self.1 <= other.1 {
+            other.1 - self.1
+        } else {
+            self.1 - other.1
+        };
+        xdiff + ydiff
+    }
+    fn successors(&self) -> [(Pos, u32); 4] {
+        let &Pos(x, y) = self;
+        [(Pos(x+1, y), 1), (Pos(x-1, y), 1), (Pos(x, y+1), 1), (Pos(x, y-1), 1)]
+    }*/
+}
+
+pub struct InitState;
+
+impl SimpleState for InitState {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        println!("Game started!");
+        data.world.create_entity()
+            .with(Pos(1,1))
+            .build();
+
+        let mut curses = EasyCurses::initialize_system().expect("Failed to start ncurses.");
+        curses.set_input_mode(InputMode::Character);
+        curses.set_keypad_enabled(true);
+        curses.set_echo(false);
+        curses.set_cursor_visibility(CursorVisibility::Invisible);
+        curses.set_input_timeout(TimeoutMode::Immediate);
+        #[cfg(unix)]
+        unsafe{ ncurses::ll::set_escdelay(0) };
+
+        curses.refresh();
+
+        data.world.insert(Curses(curses));
+    }
+
+    fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        //println!("Hello from Amethyst!");
+        Trans::None
+    }
+}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let assets_dir = app_root.join("assets/");
+
+    let game_data = GameDataBuilder::default()
+        .with(CursesInputSystem, "curses_input", &[])
+        .with(LayerVisibilityChangeSystem::default(), "layer_move", &["curses_input"])
+        .with(CursorMoveSystem::default(), "cursor_move", &["curses_input", "layer_move"])
+        .with(CursesRenderSystem, "curses_render", &["cursor_move", "layer_move"]);
+    //let start = std::time::Instant::now();
+    //astar::astar(&Pos(1, 1), |p| p.successors().into_iter().cloned(), |p| p.distance_bad(&GOAL), |p| *p == GOAL);
+    //println!("{}ms", start.elapsed().as_millis());
+    let mut game = Application::build(assets_dir, InitState)?
+        .with_frame_limit(
+            FrameRateLimitStrategy::SleepAndYield(Duration::from_millis(2)),
+            60,
+        )
+        .build(game_data)?;
+    game.run();
+    Ok(())
+}
+
