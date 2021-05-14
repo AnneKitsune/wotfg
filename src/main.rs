6fa0349e9ccfb0466a7f917ebf65c3e3aaa38a7c
jojolepro <jojolepro@jojolepro.com>
2021-05-14 11:29:05 -0400

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -273,7 +273,9 @@ pub fn entity_curses_render_system(
             && pos.chunk_y() == cursor.0.chunk_y()
             && pos.z() == cursor.0.z()
         {
-            if let Some((screen_x, screen_y)) = render.position_to_main_area(cursor, (pos.x() as u32, pos.y() as u32)) {
+            if let Some((screen_x, screen_y)) =
+                render.position_to_main_area(cursor, (pos.x() as u32, pos.y() as u32))
+            {
                 curses.move_rc(screen_y as i32, screen_x as i32);
                 curses.set_color_pair(rend.color);
                 curses.print_char(rend.render_char);
