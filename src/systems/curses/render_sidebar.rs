a4caaa3d156059742b5cde46cc2000a1cc6e1179
jojolepro <jojolepro@jojolepro.com>
2021-05-26 09:17:05 -0400

    add border to sidebar.

--- src/systems/curses/render_sidebar.rs
+++ src/systems/curses/render_sidebar.rs
@@ -12,26 +12,37 @@ pub fn curses_render_sidebar_system(
     let curses = &mut curses.as_mut().unwrap().0;
 
     let left = render.screen_width as i32 - MAIN_AREA_MARGIN_RIGHT as i32;
-    let right = render.screen_width as i32;
+    let right = render.screen_width as i32 - 1;
     let center = left + (right - left) / 2;
 
     // clear anything that could have been written on top of the sidebar
     curses.set_color_pair(*COLOR_NORMAL);
     for y in 0..render.screen_height as i32 {
-        for x in left..right {
+        for x in left..=right {
             curses.move_rc(y, x);
-            curses.print_char(' ');
+            let c = if (x == left && y == 1) || (x == right && y == 1) || (x == left && y == (render.screen_height as i32 - 1)) || (x == right && y == (render.screen_height as i32 - 1)) {
+                '+'
+            } else if x == left || x == right {
+                '|'
+            } else if y == 1 || y == (render.screen_height as i32 - 1) {
+                '-'
+            } else {
+                ' '
+            };
+            curses.print_char(c);
         }
     }
 
-    curses.move_rc(0, center - 3);
+    let mut y = 0;
+    curses.move_rc(y, center - 3);
     curses.print("Status");
-    for x in left..right {
-        curses.move_rc(0, x);
+    y += 1;
+    for x in (left + 1)..right {
+        curses.move_rc(y, x);
         curses.print_char('=');
     }
+    y += 1;
 
-    let mut y = 2;
 
     for (player, statset) in join!(&players && &stats) {
         let statset = statset.unwrap();
